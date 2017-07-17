01d766b49d3d47ad36da1b1cbb20c3e940897d0a
johnwdevine85 <Johnwdevine85@gmail.com>
2017-07-17 15:32:55 -0500

    test

---
+++ main.js
@@ -0,0 +1,46 @@
+//###################################################################
+//        __
+//   ____/ /___  ____ ______
+//  / __  / __ \/ __ `/ ___/
+// / /_/ / /_/ / /_/ (__  )
+// \__,_/\____/\__, /____/
+//            /____/
+
+// Dog Constructor & Prototype
+function Dog (dogname, status, owner, hunger, color) {
+    this.dogname = this
+    this.status = status
+    this.owner = owner
+    this.hunger = hunger
+    this.color = color
+    this.petting = function () {
+      return this.status = happy
+  }
+
+const sadie = new Dog()
+sadie.status = 'normal'
+sadie.color = 'black'
+sadie.owner = 'mason'
+console.log(sadie)
+
+const petting = new Dog()
+console.log()
+// Instances of Dog
+// Needed: sadie, moonshine, atticus
+
+//     __
+//    / /_  __  ______ ___  ____ _____  _____
+//   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
+//  / / / / /_/ / / / / / / /_/ / / / (__  )
+// /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
+
+// Human Constructor & Prototype
+function Human (humanname, status, ownedpet) {
+    this.name = name
+    this.status = status
+    this.ownedpet = pet
+    this.petting = function (dog,humanname) {
+      return this.status = happy
+  }
+// Instances of Human
+// Needed: mason, julia
